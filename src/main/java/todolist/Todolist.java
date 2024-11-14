e33d31f4b6e53dc10fa39ca425a7df9e5c934e60
RistoKuy <aristo.baadi@gmail.com>
2024-11-14 16:54:22 +0700

    Initialize Maven project with Todolist class

---
+++ src/main/java/todolist/Todolist.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package todolist;
+
+/**
+ *
+ * @author Aristo Baadi
+ */
+public class Todolist {
+
+    public static void main(String[] args) {
+        
+    }
+}
